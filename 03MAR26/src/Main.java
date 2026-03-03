577f81bc6f76baa92c3a21e125519e60200e0821
Joshua Reece <joshua.t.reece.mil@army.mil>
2026-03-03 14:48:51 -0600

    Start of recursion practice

--- 03MAR26/src/Main.java
+++ 03MAR26/src/Main.java
@@ -5,11 +5,12 @@ public class Main {
         System.out.print(fact(5));
 
     }
-;
+
     public static int fact(int n) {
 
         if(n==1)
             return 1;
-        return n*fact(n-1);
+
+        return n * fact(n-1);
     }
 }
